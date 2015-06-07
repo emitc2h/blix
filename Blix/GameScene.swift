9f181e4fe44dcf812449452e8e909f9370173c24
Michel Trottier-McDonald <emitc2h@gmail.com>
2015-06-07 18:29:28 +0200

    Worked out the geometrical challenges. Rest is implementation.

--- Blix/GameScene.swift
+++ Blix/GameScene.swift
@@ -13,51 +13,64 @@ import Foundation
 // Game Scene class
 // ********************************************
 class GameScene: SKScene {
-    
-    var lattice: Lattice
-    var center: CGPoint
-    var tileCenter: CGPoint
-    var h: CGFloat
-    var w: CGFloat
-    
-    // --------------------------------------------
-    // Constructors
-    // --------------------------------------------
-    override init(size: CGSize) {
-        
-        // Record scene size
-        w = size.width
-        h = size.height
-        
-        center = CGPoint(x: w/2, y: h/2 + 90)
-        tileCenter = CGPoint(x: w/2, y: h/2 + 22.5)
-        
-        lattice = Lattice(position: CGPoint(x:0,y:0), nx: 19, ny: 34, hexSize: 10, spacing: 1)
-        super.init(size: size)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 
-
+    var background: SKSpriteNode!
+    var tile: SKSpriteNode!
+    var tileContainer: SKNode!
+    var tileSuperContainer: SKNode!
+    var initialTouchPosition: CGPoint!
+    var currentTouchPosition: CGPoint!
+    var finalTouchPosition: CGPoint!
+    var tileHeight: CGFloat!
+    var tileWidth: CGFloat!
+    
     // --------------------------------------------
     // When you reach the view
     // --------------------------------------------
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        initBackground()
+        initTile()
+
+    }
+    
+    
+    // --------------------------------------------
+    // initialize background
+    // --------------------------------------------
+    func initBackground() {
         
-        // Set background
-        //let background = SKSpriteNode(imageNamed: "background")
-        //background.position = CGPoint(x: 0, y: 0)
-        //background.anchorPoint = CGPoint(x: 0, y: 0)
-        //addChild(background)
-        backgroundColor = UIColor.whiteColor()
+        background = SKSpriteNode(imageNamed: "background")
+        background.anchorPoint = CGPoint(x: 0,y: 0)
+        background.position = CGPoint(x:0, y:0)
+        addChild(background)
         
-        addChild(lattice)
+    }
+    
+    
+    // --------------------------------------------
+    // initialize tile
+    // --------------------------------------------
+    func initTile() {
         
+        tile = SKSpriteNode(imageNamed: "tile")
+        tile.anchorPoint = CGPoint(x: 0.5,y: 0.5)
+        tile.blendMode = .Alpha
+        tileHeight = tile.size.height
+        tileWidth  = tile.size.width
         
-        //
+        tileContainer = SKNode()
+        tileContainer.addChild(tile)
+        
+        tileSuperContainer = SKNode()
+        tileSuperContainer.addChild(tileContainer)
+        
+        tileSuperContainer.position = CGPoint(x: size.width/2, y: size.height/2)
+        tileContainer.position = CGPoint(x: 0.0, y: -tileHeight/2)
+        tile.position = CGPoint(x: 0.0, y: tileHeight/2)
+        
+        addChild(tileSuperContainer)
+        
     }
     
     
@@ -66,9 +79,9 @@ class GameScene: SKScene {
     // --------------------------------------------
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
 
-        // turn hexagons blue
         var touch = touches.first as! UITouch
-        let positionInScene = touch.locationInNode(self)
+        initialTouchPosition = CGPoint(x: size.width/2, y: size.height/2) //touch.locationInNode(self)
+        
     }
     
     
@@ -76,6 +89,27 @@ class GameScene: SKScene {
     // follow touch
     // --------------------------------------------
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        var touch = touches.first as! UITouch
+        currentTouchPosition = touch.locationInNode(self)
+        
+        tile.zRotation = hexagonSideToBottom(initialTouchPosition, currentTouchPosition)
+        
+        //let newAnchorPoint = anchorPointFromTouches(initialTouchPosition, currentTouchPosition)
+        //changeAnchorkeepPosition(tile, newAnchorPoint)
+        
+        var y_scale = (tileHeight - (initialTouchPosition - currentTouchPosition).mag() * 2.0) / tileHeight
+        
+        if y_scale < -1.0 {
+            y_scale = -1.0
+        }
+        
+        tile.color = UIColor.blackColor()
+        tile.colorBlendFactor = 1.0 - fabs(y_scale)
+        
+        tileContainer.yScale = y_scale
+        
+        tileSuperContainer.zRotation = -tile.zRotation
         
     }
     
@@ -85,6 +119,19 @@ class GameScene: SKScene {
     // --------------------------------------------
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         
+        var touch = touches.first as! UITouch
+        finalTouchPosition = touch.locationInNode(self)
+        
+        tile.colorBlendFactor = 0.0
+        
+        tile.zRotation = 0.0
+        tileContainer.zRotation = 0.0
+        tileSuperContainer.zRotation = 0.0
+        //changeAnchorkeepPosition(tile, CGPoint(x:0.5, y:0.5))
+        
+        tileContainer.yScale = 1.0
+        tileContainer.xScale = 1.0
+        
     }
     
     
@@ -93,6 +140,5 @@ class GameScene: SKScene {
     // --------------------------------------------
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-        self.lattice.update(currentTime)
     }
 }
--- Blix/Utilities.swift
+++ Blix/Utilities.swift
@@ -9,26 +9,163 @@
 import Foundation
 import SpriteKit
 
+
 // Constants
+// --------------------------------------------------------
 let cos30 = cos(M_PI/6)
 let sin30 = sin(M_PI/6)
 
+
 // Add 2D vectors
+// --------------------------------------------------------
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 
+
 // Subtract 2D vectors
+// --------------------------------------------------------
 func - (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x - right.x, y: left.y - right.y)
 }
 
+
 // Multiply 2D vectors by a scalar
+// --------------------------------------------------------
 func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
     return CGPoint(x: point.x * scalar, y: point.y * scalar)
 }
 
+
 // Divide 2D vector by a scalar
+// --------------------------------------------------------
 func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
     return CGPoint(x: point.x / scalar, y: point.y / scalar)
 }
+
+
+// Vector magnitude
+// --------------------------------------------------------
+extension CGPoint {
+    func mag () -> CGFloat {
+        return sqrt(self.x*self.x + self.y*self.y)
+    }
+}
+
+
+// --------------------------------------------------------
+extension CGFloat {
+    func degrees_to_radians() -> CGFloat {
+        return CGFloat(M_PI) * self / 180.0
+    }
+}
+
+
+// --------------------------------------------------------
+func hexagonSideFromTouches (initialTouch: CGPoint, finalTouch: CGPoint) -> Int {
+    
+    let vector = finalTouch - initialTouch
+    return (Int(floor(atan2(vector.x, vector.y) / CGFloat(M_PI/3.0))) + 6) % 6
+}
+
+
+// --------------------------------------------------------
+func anchorPointFromTouches (initialTouch: CGPoint, finalTouch: CGPoint) -> CGPoint {
+    let side = hexagonSideFromTouches (initialTouch, finalTouch)
+    
+    switch side {
+    case 0:
+        return CGPoint(x: 0.750, y: 0.875)
+    case 1:
+        return CGPoint(x: 1.000, y: 0.500)
+    case 2:
+        return CGPoint(x: 0.750, y: 0.125)
+    case 3:
+        return CGPoint(x: 0.250, y: 0.125)
+    case 4:
+        return CGPoint(x: 0.000, y: 0.500)
+    case 5:
+        return CGPoint(x: 0.250, y: 0.875)
+    default:
+        return CGPoint(x: 0.500, y: 0.500)
+    }
+}
+
+
+// --------------------------------------------------------
+func angleFromTouches (initialTouch: CGPoint, finalTouch: CGPoint) -> CGFloat {
+    
+    let vector = finalTouch - initialTouch
+    let angle = atan2(vector.x, vector.y)
+    return CGFloat(-angle)
+}
+
+
+// --------------------------------------------------------
+func hexagonSideToBottom (initialTouch: CGPoint, finalTouch: CGPoint) -> CGFloat {
+    
+    let side = hexagonSideFromTouches (initialTouch, finalTouch)
+    
+    switch side {
+    case 5:
+        return CGFloat(5  * M_PI/6)
+    case 4:
+        return CGFloat(3  * M_PI/6)
+    case 3:
+        return CGFloat(1  * M_PI/6)
+    case 2:
+        return CGFloat(11 * M_PI/6)
+    case 1:
+        return CGFloat(9  * M_PI/6)
+    case 0:
+        return CGFloat(7  * M_PI/6)
+    default:
+        return CGFloat()
+    }
+    
+}
+
+
+// --------------------------------------------------------
+func changeAnchorkeepPosition (node: SKSpriteNode, newAnchorPoint: CGPoint) {
+    
+    let dx = node.size.width * (newAnchorPoint.x - node.anchorPoint.x)
+    let dy = node.size.height * (newAnchorPoint.y - node.anchorPoint.y)
+    
+    let delta = CGPoint(x: dx, y: dy)
+    
+    node.anchorPoint = newAnchorPoint
+    node.position = node.position + delta
+}
+
+// --------------------------------------------------------
+
+
+// --------------------------------------------------------
+func rotateAboutCenter (node: SKSpriteNode, angle: CGFloat) {
+    
+    let currentAnchorPoint = node.anchorPoint
+    let center = CGPoint(x: 0.5, y: 0.5)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
